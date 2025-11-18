49b183d08995fb6e52ca15b9bbc3d8e0f90ce190
Propelor RMS Developer <developer@propelor.com>
2025-11-18 14:33:01 +0800

    fix: remove unsupported nullsLast option from Supabase order query

--- src/index.ts
+++ src/index.ts
@@ -3508,7 +3508,7 @@ async function createHttpServer() {
       const offset = (pageNum - 1) * pageSize;
 
       query = query
-        .order('contract_number', { ascending: true, nullsLast: true })
+        .order('id', { ascending: true })
         .range(offset, offset + pageSize - 1);
 
       const { data, error, count } = await query;
